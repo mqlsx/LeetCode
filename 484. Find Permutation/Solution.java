79062ae2a61a3f3096d5b76020daca35fbd2848f
mqlsx <mqlsx.ustc@gmail.com>
2018-06-02 20:33:38 +0800

    6/2/2018

--- 484. Find Permutation/Solution.java
+++ 484. Find Permutation/Solution.java
@@ -1,20 +1,39 @@
-// 484. Find Permutation
+// 484.Find Permutation
+
+
 
 public class Solution {
 
+
+
     public int[] findPermutation(String s) {
+
         // write your code here
+
         int[] result = new int[s.length() + 1];
+
         
+
         int index = 0;
+
         for (int i = 0; i <= s.length(); i++) {
+
             if (i == s.length() || s.charAt(i) == 'I') {
+
                 for (int j = i + 1; index <= i; index++) {
+
                     result[index] = j;
+
                     j--;
+
                 }
+
             }
+
         }
+
         return result;
+
     }
+
 }
