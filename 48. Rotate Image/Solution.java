f57c50f02af0361865156bccad405d9e58e36faa
mqlsx <mqlsx.ustc@gmail.com>
2018-06-19 09:57:47 +0800

    6/19/2018

---
+++ 48. Rotate Image/Solution.java
@@ -0,0 +1,16 @@
+//48. Rotate Image
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0; i < n / 2; i++) {
+            for(int j = i; j < n - 1 - i; j++) {
+                int temp = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 -j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
