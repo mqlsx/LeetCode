ac88ae1d1a50bb909244abd8cab8f0e5b4624c81
mqlsx <mqlsx.ustc@gmail.com>
2018-07-03 19:17:34 +0800

    7/3/2018

---
+++ 10. Regular Expression Matching/Solution.java
@@ -0,0 +1,28 @@
+//10. Regular Expression Matching
+
+
+// dp, O(mn) time, O(mn) space:
+// this can be optimized to O(n) space as well
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;//remember to initialize dp[0][0]: "" matches "" is true !!!
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (i > 0 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') && dp[i - 1][j]);
+                } else {
+                    dp[i][j] = i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') && dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+
+
